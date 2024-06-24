8cf4091361ab9a0ca4a1a7a246781224327c31ca
christineca <ccarandang@enova-me.com>
2024-06-24 08:56:31 +0400

    Update main.py

--- project/main.py
+++ project/main.py
@@ -2,7 +2,19 @@ import pandas as pd
 from data_extraction.dummy_data_extractor import extract_dummy_data
 from data_extraction.skyspark_data_extractor import extract_data
 from models.seasonalNaive import seasonalNaive
-
+#_______________________________
+import matplotlib.pyplot as plt
+import numpy as np
+from pandas.plotting import scatter_matrix
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.experimental import enable_iterative_imputer  # noqa
+from sklearn.impute import IterativeImputer, SimpleImputer
+from sklearn.kernel_approximation import Nystroem
+from sklearn.linear_model import BayesianRidge, Ridge
+from sklearn.model_selection import cross_val_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.pipeline import make_pipeline
+#_______________________________
 
 master_table = extract_dummy_df("C:\Users\nramirez\OneDrive - Enova Facilities Management\Documents\enovaDataCleaning\HubgradeDataCleaning\dummy_data")
 
