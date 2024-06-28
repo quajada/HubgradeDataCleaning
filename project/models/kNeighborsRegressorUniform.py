9d0ad4c266268b5c2f93f5df65e10b70015faca0
christineca <ccarandang@enova-me.com>
2024-06-28 16:19:36 +0400

    commit

--- project/models/kNeighborsRegressorUniform.py
+++ project/models/kNeighborsRegressorUniform.py
@@ -2,7 +2,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
 import pandas as pd
 
-def kNeighbors_Regressor(master_table):
+def kNeighbors_Regressor_Uniform(master_table):
     """
     Input
     master_table: main table received from SS
@@ -33,7 +33,7 @@ def kNeighbors_Regressor(master_table):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
     # # Apply KNN regression
-    knn_regressor = KNeighborsRegressor(n_neighbors=3)
+    knn_regressor = KNeighborsRegressor(n_neighbors=3,weights="uniform")
     knn_regressor.fit(X_train, y_train)
     predictions = knn_regressor.predict(X_test)
     predictions
