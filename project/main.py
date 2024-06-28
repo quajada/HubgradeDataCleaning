ea06cfc7728fa7b472150fd44247e25dff6e3f10
christineca <ccarandang@enova-me.com>
2024-06-28 10:35:04 +0400

    Update main.py

--- project/main.py
+++ project/main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from data_extraction.dummy_data_extractor import extract_dummy_data
-from data_extraction.skyspark_data_extractor import extract_data
+from data_extraction.skyspark_data_extractor import extractData
 from models.seasonalNaive import seasonalNaive
 from models.dynamic_optimized_theta import dynamic_optimized_theta
 
