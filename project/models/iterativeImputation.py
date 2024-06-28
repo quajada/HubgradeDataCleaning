ed69de2106a967e234949acb070a1c8746cc8fdb
christineca <ccarandang@enova-me.com>
2024-06-28 11:07:09 +0400

    Update iterativeImputation.py

--- project/models/iterativeImputation.py
+++ project/models/iterativeImputation.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from sklearn.impute import IterativeImputer
 from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
 
 def iterative_Imputation(master_table):
     """
