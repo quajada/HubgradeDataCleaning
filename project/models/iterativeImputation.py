d1ab7e4aebacadd4e75c4bb57f273596b916da71
christineca <ccarandang@enova-me.com>
2024-06-28 10:49:16 +0400

    commit

--- project/models/iterativeImputation.py
+++ project/models/iterativeImputation.py
@@ -1,69 +1,20 @@
 import pandas as pd
+from sklearn.impute import IterativeImputer
 
-def seasonal_naive(df, length_of_missing_data, data_logging_interval):
+def iterative_Imputation(master_table):
     """
-    Inputs
-    df: df used for training set (from SS)
-    length_of_missing_data: interval length of missing data (from SS)
-    data_logging_interval: data logging interval - called from the hisDQInterval tag on the point (from SS)
+    Input
+    master_table: main table received from SS
 
     Output
-    forecasts_df: dataframe with predictions for the period missing data. Index names as ts, values column named as "v0
+    mt_predict: dataframe with predictions for all rows with missing columns. Index names as ts
     """
+    master_table = master_table.at[0,"his"]
+    mt = master_table.set_index(["ts"])
+    mt["status"] = mt.isna().any(axis=1)
+    imputer = IterativeImputer()
+    imputed = imputer.fit_transform(mt)
+    mt_imputed = pd.DataFrame(imputed, index=mt.index, columns=mt.columns)
+    mt_predict = mt_imputed[mt_imputed["status"]==1]
     
-<<<<<<< Updated upstream
-
-    # step 1 convert the grid to a dataframe, and set first column as index     ### UNCOMMENT THIS ONLY IF RUNNING THE MODEL DIRECTLY ON SS. THIS IS DONE IN THE ENSEMBLE MODEL SO NO NEED TO HAVE THIS WHEN RUNNING THROUGH ENSEMBLE MODEL
-    #df = df.to_dataframe()
-    #df.set_index(df.columns[0], inplace=True, drop=True)
-=======
-    # step 1 convert the grid to a dataframe, and set first column as index
-    df = df.to_dataframe()
-    df.set_index(df.columns[0], inplace=True, drop=True)
->>>>>>> Stashed changes
-
-    # rename the first column as "target"
-    new_column_name = "target"
-    df = df.rename(columns={df.columns[0]: new_column_name})
-
-    # format the df to statsforecast format
-    df = df.reset_index()
-    df = df.rename(columns={df.columns[0]: 'ds', df.columns[1]: "y"})
-    df['unique_id'] = "v0"    
-
-    # number of predictions
-    horizon = int(length_of_missing_data/data_logging_interval) + 1 # why -1? because if you do length_of_missing_data/data_logging_interval you will get prediction length that is exclusive of the start ts (start ts is the last ts with actual data before the gap), and inclusive of the end ts (end ts is the first ts with actual data after the gap). +1 to get predictions also for the start and end timestamp. Can remove them later
-
-    # season length
-    season_length = int(pd.Timedelta(24, 'h') / data_logging_interval)      
-
-    # frequency
-    freq = str(data_logging_interval.total_seconds()/3600)+"h"
-
-
-    # LIST OF MODELS
-    models = [
-        SeasonalNaive(season_length=season_length) 
-    ]
-
-    # The Model
-    sf = StatsForecast( 
-        models=models,
-        freq=freq, 
-        # fallback_model = SeasonalNaive(season_length=season_length),
-        n_jobs=-1,
-    )
-
-    # Model fitting
-    forecasts_df = sf.forecast(df=df[["ds", "y", "unique_id"]], h=horizon, level=[90])  
-
-    # removing the -hi- and -lo- columns
-    for col in forecasts_df.columns:
-        if re.search("-hi-", col) or re.search("-lo-", col):
-            forecasts_df.drop(col, axis=1, inplace=True)
-            
-    forecasts_df = forecasts_df.rename(columns={"ds": "timestamp"})
-
-    forecasts_df.set_index("timestamp", inplace=True)
-
-    return forecasts_df
+    return mt_predict
